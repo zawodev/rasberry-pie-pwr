4f87ce4dfbb82af826ddf8d6532d3dccd937c18a
zawo <alikstepaniuk@gmail.com>
2024-12-10 23:08:41 +0100

    aaaa

--- lab09/zad2.py
+++ lab09/zad2.py
@@ -1,130 +1,127 @@
 #pip install adafruit-circuitpython-neopixel adafruit-circuitpython-bme280
-
 import time
+import os
 import board
-import busio
-import adafruit_bme280
-import adafruit_ds18x20
-import adafruit_onewire.bus
 import neopixel
+import RPi.GPIO as GPIO
+import w1thermsensor
+import busio
+import adafruit_bme280.advanced as adafruit_bme280
+from config import *
 
-# Konfiguracja diod WS2812
-pixel_pin = board.D18
-num_pixels = 30
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False)
+# WS2812 config
+LED_COUNT = 8
+LED_PIN = ws2812pin
+LED_BRIGHTNESS = 1.0 / 32
+pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False)
 
-# Konfiguracja czujnika BME280
-i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+# ds18b20
+def ds18b20():
+    sensor = w1thermsensor.W1ThermSensor()
+    temp = sensor.get_temperature()
+    return temp
 
-# Konfiguracja czujnika DS18B20
-ow_bus = adafruit_onewire.bus.OneWireBus(board.D4)
-ds18b20 = adafruit_ds18x20.DS18X20(ow_bus.scan()[0])
+# bme280
+def bme280():
+    i2c = busio.I2C(board.SCL, board.SDA)
+    bme = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
 
-def read_sensors():
-    temperature = ds18b20.temperature
-    humidity = bme280.humidity
-    pressure = bme280.pressure
-    return temperature, humidity, pressure
+    bme.sea_level_pressure = 1013.25
+    bme.standby_period = adafruit_bme280.STANDBY_TC_500
+    bme.iir_filter = adafruit_bme280.IIR_FILTER_X16
+    bme.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+    bme.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+    bme.overscan_temperature = adafruit_bme280.OVERSCAN_X2
 
-def visualize_data(temperature, humidity, pressure):
-    # Przykładowa wizualizacja: zmiana koloru diod w zależności od temperatury
-    for i in range(num_pixels):
-        if temperature < 20:
-            pixels[i] = (0, 0, 255)  # Niebieski
-        elif 20 <= temperature < 25:
-            pixels[i] = (0, 255, 0)  # Zielony
-        else:
-            pixels[i] = (255, 0, 0)  # Czerwony
+    return bme
+
+# visualize temparature on WS2812
+def visualize_temperature(temp):
+    temp_color = (0, 0, 255) if temp < 20 else (255, 255, 0) if temp < 30 else (255, 0, 0)
+    intensity = min(max(int(temp / 5), 1), LED_COUNT)
+    for i in range(LED_COUNT):
+        pixels[i] = temp_color if i < intensity else (0, 0, 0)
     pixels.show()
 
-def main():
-    while True:
-        temperature, humidity, pressure = read_sensors()
-        print(f"Temperature: {temperature:.2f} C, Humidity: {humidity:.2f} %, Pressure: {pressure:.2f} hPa")
-        visualize_data(temperature, humidity, pressure)
-        time.sleep(1)
-
-if __name__ == "__main__":
-    main()
-    
-    
-    
-    
-# pip install adafruit-circuitpython-neopixel adafruit-circuitpython-bme280 adafruit-circuitpython-encoder adafruit-circuitpython-mcp230xx
-import time
-import board
-import busio
-import adafruit_bme280
-import adafruit_ds18x20
-import adafruit_onewire.bus
-import neopixel
-import digitalio
-from adafruit_mcp230xx.mcp23017 import MCP23017
-from adafruit_seesaw import seesaw, rotaryio, digitalio as seesaw_digitalio
-
-# Configuration for WS2812 LEDs
-pixel_pin = board.D18
-num_pixels = 30
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False)
-
-# Configuration for BME280 sensor
-i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-
-# Configuration for DS18B20 sensor
-ow_bus = adafruit_onewire.bus.OneWireBus(board.D4)
-ds18b20 = adafruit_ds18x20.DS18X20(ow_bus.scan()[0])
-
-# Configuration for buttons and encoder
-mcp = MCP23017(i2c)
-button1 = mcp.get_pin(0)
-button2 = mcp.get_pin(1)
-button1.direction = digitalio.Direction.INPUT
-button1.pull = digitalio.Pull.UP
-button2.direction = digitalio.Direction.INPUT
-button2.pull = digitalio.Pull.UP
-
-seesaw = seesaw.Seesaw(i2c, addr=0x36)
-encoder = rotaryio.IncrementalEncoder(seesaw)
-last_position = None
-
-def read_sensors():
-    temperature = ds18b20.temperature
-    humidity = bme280.humidity
-    pressure = bme280.pressure
-    return temperature, humidity, pressure
-
-def visualize_data(temperature, humidity, pressure):
-    for i in range(num_pixels):
-        if temperature < 20:
-            pixels[i] = (0, 0, 255)  # Blue
-        elif 20 <= temperature < 25:
-            pixels[i] = (0, 255, 0)  # Green
-        else:
-            pixels[i] = (255, 0, 0)  # Red
+# visualize humidity on WS2812
+def visualize_humidity(humidity):
+    humidity_color = (0, 255, 0)
+    intensity = min(max(int(humidity / 20), 1), LED_COUNT)
+    for i in range(LED_COUNT):
+        pixels[i] = humidity_color if i < intensity else (0, 0, 0)
     pixels.show()
 
-def main():
-    global last_position
-    while True:
-        temperature, humidity, pressure = read_sensors()
-        print(f"Temperature: {temperature:.2f} C, Humidity: {humidity:.2f} %, Pressure: {pressure:.2f} hPa")
-        visualize_data(temperature, humidity, pressure)
+# visualize pressure on WS2812
+def visualize_pressure(pressure):
+    pressure_color = (0, 255, 255)
+    intensity = min(max(int((pressure - 900) / 50), 1), LED_COUNT)
+    for i in range(LED_COUNT):
+        pixels[i] = pressure_color if i < intensity else (0, 0, 0)
+    pixels.show()
 
-        # Check button states
-        if not button1.value:
-            print("Button 1 pressed")
-        if not button2.value:
-            print("Button 2 pressed")
+# button handler
+def button_pressed_callback(channel):
+    global current_mode
+    if channel == buttonRed:
+        current_mode = (current_mode - 1) % 3
+    elif channel == buttonGreen:
+        current_mode = (current_mode + 1) % 3
 
-        # Check encoder position
-        position = -encoder.position
-        if position != last_position:
-            print(f"Encoder position: {position}")
-            last_position = position
+# encoder handler
+def read_encoder():
+    global current_mode
+    left_state = GPIO.input(encoderLeft)
+    right_state = GPIO.input(encoderRight)
 
-        time.sleep(1)
+    if left_state == GPIO.LOW:
+        time.sleep(0.01)  # debounce delay
+        if GPIO.input(encoderLeft) == GPIO.LOW:
+            if right_state == GPIO.HIGH:
+                current_mode = (current_mode + 1) % 3  # Next mode
+            else:
+                current_mode = (current_mode - 1) % 3  # Previous mode
 
+# main
 if __name__ == "__main__":
-    main()
+    try:
+        print("\nprogram start")
+
+        # rejestracja eventów dla przycisków i enkodera
+        # GPIO.add_event_detect(buttonRed, GPIO.FALLING, callback=button_pressed_callback, bouncetime=200)
+        GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=button_pressed_callback, bouncetime=200)     
+        
+        current_mode = 0  # (0 - temp, 1 - humid, 2 - press)
+
+        while True:
+            if GPIO.input(buttonRed) == GPIO.LOW:
+                print("program exit")
+                break
+            read_encoder()  # read encoder
+
+            if current_mode == 0: # temperature
+                temp = ds18b20()
+                print(f"temperature: {temp} °C")
+                visualize_temperature(temp)
+
+            elif current_mode == 1: # humidity
+                bme = bme280()
+                humidity = bme.humidity
+                print(f"humidity: {humidity} %")
+                visualize_humidity(humidity)
+
+            elif current_mode == 2: # pressure
+                bme = bme280()
+                pressure = bme.pressure
+                print(f"pressure: {pressure} hPa")
+                visualize_pressure(pressure)
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("\program interrupt")
+
+    finally:
+        pixels.fill((0, 0, 0))
+        pixels.show()
+        GPIO.cleanup()
+        print("\nprogram finish")
